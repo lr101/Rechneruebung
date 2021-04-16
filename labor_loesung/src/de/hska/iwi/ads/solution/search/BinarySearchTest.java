e23a50a51f88a143c33d0476f30611148acc05c3
lr101 <48615489+lr101@users.noreply.github.com>
2021-04-16 14:47:47 +0200

    Add working binarySearch

---
+++ labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearchTest.java
@@ -0,0 +1,12 @@
+package de.hska.iwi.ads.solution.search;
+
+import de.hska.iwi.ads.search.Search;
+import de.hska.iwi.ads.search.SearchTest;
+
+public class BinarySearchTest extends SearchTest {
+
+    @Override
+    public <E extends Comparable<E>> Search<E> createSearch() {
+        return new BinarySearch<>();
+    }
+}
